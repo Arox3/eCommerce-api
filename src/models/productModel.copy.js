dc353c1e1d2d7faa8a3f279ff05495a2061bb195
Tanapol Sriyotee <tanapol.sr@inet.co.th>
2024-01-17 21:08:54 +0700

    add new product model

---
+++ src/models/productModel.copy.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
+
+const productSchema = new mongoose.Schema(
+  {
+    productName: { type: String, default: null },
+    description: { type: String, default: null },
+    price: { type: Number, default: null },
+    productImage: { type: String, default: null },
+  },
+  { timestamps: true }
+);
+
+productSchema.plugin(AutoIncrement, {
+  inc_field: "id",
+  id: "productIdNums",
+  start_seq: 500,
+});
+
+module.exports = mongoose.model("product", productSchema);
